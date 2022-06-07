4af8e2ffa25eb6b6eacae3a1517b897f241314fb
Ivor Picek <ivor@krtech.digital>
2022-06-07 15:33:37 +0200

    Implemented button component storybook

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,41 +1,51 @@
 import React from "react"
-import Button from "../src/components/Button/Button"
+import Button from "../src/components/Button/Button.tsx"
 
 
 
 export default{
-    title: "Form/Button",
+    title: "Button",
     component: Button,
-    args: {
-        children: "Button"
-    }
-
 }
 
-export const Primary = () => <Button variant="primary">Primary</Button>
-export const Secondary = () => <Button variant="secondary">Secondary</Button>
-export const Success = () => <Button variant="success">Success</Button>
-export const Danger = () => <Button variant="danger">Danger</Button>
 
 
-// Bolje koristiti dok je komponenta sa više stateova. 
-
-const Template = args => <Button {...args}> </Button>
-
-export const PrimaryA = Template.bind({})
-PrimaryA.args = {
-    variant: "primary",
-    // children: "Primary Args"
+export function Normal () {
+    return (
+        <>
+        <Button variant="none">{`Item`}</Button>
+        <Button variant="none">{`Item  >`}</Button>
+        <Button variant="none">{`<  Item`}</Button>
+        <Button variant="none">{`<  Item  >`}</Button>
+        <Button variant="noneShort">{`>`}</Button>
+        </>
+    )
 }
 
-export const LongPrimaryA = Template.bind({})
-LongPrimaryA.args = {
-    ...PrimaryA.args,
-    // children: "Long Primary Args"
+export function Hover () {
+    return (
+        <>
+        <Button variant="hover">{`Item`}</Button>
+        <Button variant="hover">{`Item  >`}</Button>
+        <Button variant="hover">{`<  Item`}</Button>
+        <Button variant="hover">{`<  Item  >`}</Button>
+        <Button variant="hoverShort">{`>`}</Button>
+        </>
+    )
 }
 
-export const SecondaryA = Template.bind({})
-    SecondaryA.args = {
-        variant: "Secondary",
-        // children: "Secondary Args"
-    }
+export function Active () {
+    return (
+        <>
+        <Button variant="active">{`Item`}</Button>
+        <Button variant="active">{`Item  >`}</Button>
+        <Button variant="active">{`<  Item`}</Button>
+        <Button variant="active">{`<  Item  >`}</Button>
+        <Button variant="activeShort">{`>`}</Button>
+        </>
+    )
+}
+
+
+
+
