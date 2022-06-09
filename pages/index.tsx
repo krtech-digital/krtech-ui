52e4a326614781ac1d7e96cca973b6d6d5d10860
CROmartin <cromartin11@gmail.com>
2022-06-09 17:58:02 +0200

    setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@krtech-digital/krtech-ui";
-
+// import Button from "../src/components/Button/Button";
 const Index = () => {
   return (
     <div className="App">
       <a href="/">Homepage</a>
       <br />
       <a href="/about">Components</a>
+      <Button variant="none">Click</Button>
       <br />
       <br />
     </div>
---
+++ stories/buttonTest.stories.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Button from "../src/components/Button/Button.tsx";
+
+export default {
+  title: "Button test",
+  component: Button,
+};
+
+export function Normal() {
+  return (
+    <>
+      <Button variant="none">{`Item`}</Button>
+      <Button variant="none">{`Item  >`}</Button>
+      <Button variant="none">{`<  Item`}</Button>
+      <Button variant="none">{`<  Item  >`}</Button>
+      <Button variant="noneShort">{`>`}</Button>
+    </>
+  );
+}
+
+export function Hover() {
+  return (
+    <>
+      <Button variant="hover">{`Item`}</Button>
+      <Button variant="hover">{`Item  >`}</Button>
+      <Button variant="hover">{`<  Item`}</Button>
+      <Button variant="hover">{`<  Item  >`}</Button>
+      <Button variant="hoverShort">{`>`}</Button>
+    </>
+  );
+}
+
+export function Active() {
+  return (
+    <>
+      <Button variant="active">{`Item`}</Button>
+      <Button variant="active">{`Item  >`}</Button>
+      <Button variant="active">{`<  Item`}</Button>
+      <Button variant="active">{`<  Item  >`}</Button>
+      <Button variant="activeShort">{`>`}</Button>
+    </>
+  );
+}
