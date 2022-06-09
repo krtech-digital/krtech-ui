92922223a4a75d43fb475a3ae44ba52b96d39069
Matija Kanizaj <matija@krtech.digital>
2022-06-09 16:18:30 +0200

    Add TableCard component

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import Tile from "./components/Tile/Tile";
 import FileUpload from "./components/FileUpload/FileUpload";
 import Checkbox from "./components/Checkbox/Checkbox";
 import TextCard from "./components/Cards/TextCard/TextCard";
+import TableCard from "./components/Cards/TableCard/TableCard";
 
-export { Button, Tile, FileUpload, Checkbox, TextCard };
+export { Button, Tile, FileUpload, Checkbox, TextCard, TableCard };
