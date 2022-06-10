d50404cf8342b0ece2b2be864900d0de82a539fc
Ivor Picek <ivor@krtech.digital>
2022-06-10 10:24:03 +0200

    Added Tile Component

---
+++ stories/Tiles.stories.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Meta, Story } from "@storybook/react";
+import { Tiles, Props } from "../src/components/Tiles/Tiles";
+
+const meta: Meta = {
+  title: "Tiles",
+  component: Tiles,
+};
+
+export default meta;
+
+export const VariantASingleTileWithText = () => (
+  <Tiles
+    variant="a"
+    firstText="DOCUMENTATION"
+    secondText="Read more detail information about the project"
+    tileNumber={1}
+  />
+);
+
+export const VariantAMultipleTilesWithText = () => (
+  <Tiles
+    variant="a"
+    firstText="DOCUMENTATION"
+    secondText="Read more detail information about the project"
+    tileNumber={3}
+  />
+);
+
+export const VariantBTile = () => (
+  <Tiles
+    variant="b"
+    firstText="DOWNLOAD"
+    secondText="Documentation"
+    tileNumber={1}
+  />
+);
+
+export const VariantBMultipleTiles = () => (
+  <Tiles
+    variant="b"
+    firstText="DOWNLOAD"
+    secondText="Documentation"
+    tileNumber={3}
+  />
+);
