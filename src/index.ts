10a43f5de7f8682514ce936b078b928e6f97163d
karlotomasic9@gmail.com <karlotomasic@Karlos-MacBook-Pro.fritz.box>
2022-07-07 18:24:10 +0200

    fix: Import component to root ts file.

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import Tile from "./components/Tile/Tile";
 import FileUpload from "./components/FileUpload/FileUpload";
 import Checkbox from "./components/Checkbox/Checkbox";
 import ListChoice from "./components/ListChoice/ListChoice";
+import Modal from "./components/Modal/Modal";
 
-export { Button, Tile, FileUpload, Checkbox, ListChoice };
+export { Button, Tile, FileUpload, Checkbox, ListChoice, Modal };
