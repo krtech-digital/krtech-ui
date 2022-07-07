4f71afa85030d4a27368d98a2b710b65e72feeed
karlotomasic9@gmail.com <karlotomasic@Karlos-MacBook-Pro.fritz.box>
2022-07-07 18:53:28 +0200

    feat[new]: Add small modal component.

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import Tile from "./components/Tile/Tile";
 import FileUpload from "./components/FileUpload/FileUpload";
 import Checkbox from "./components/Checkbox/Checkbox";
 import ListChoice from "./components/ListChoice/ListChoice";
+import ModalBox from "./components/ModalBox/ModalBox";
 
-export { Button, Tile, FileUpload, Checkbox, ListChoice };
+export { Button, Tile, FileUpload, Checkbox, ListChoice, ModalBox };
