ebeffdc93eda1a296f8c1fbd41bb9952703cdc41
erik <erik.kranjec8@outlook.com>
2022-08-18 07:43:01 +0200

    Checkbox fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
-import { Button } from "@krtech-digital/krtech-ui";
+import { Button, Checkbox } from "@krtech-digital/krtech-ui";
+import React from "react";
 
 const Index = () => {
-  return (
-    <div className="App">
-      <a href="/">Homepage</a>
-      <br />
-      <a href="/about">Components</a>
-      <br />
-      <br />
-    </div>
-  );
+	return (
+		<div className="App">
+			<a href="/">Homepage</a>
+			<br />
+			<a href="/about">Components</a>
+			<br />
+			<br />
+		</div>
+	);
 };
 
 export default Index;
