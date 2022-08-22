6190680ba36af62406ea5460b9ec0efebf022428
erik <erik.kranjec8@outlook.com>
2022-08-22 13:30:23 +0200

    fixed button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,31 @@
-import React, { HTMLAttributes, ReactNode } from "react";
+import React, { HTMLAttributes } from "react";
 import style from "./Button.module.css";
 
 export interface Props extends HTMLAttributes<HTMLButtonElement> {
-  buttonText: string;
-  variant: string;
+	text: string;
+	variant: string;
+	width?: string;
+	height?: string;
+	padding?: string;
 }
 
-export const Button = ({ buttonText, variant, ...props }: Props) => {
-  return (
-    <button className={`${style.button} ${style[variant]}`} {...props}>
-      {buttonText}
-    </button>
-  );
+export const Button = ({
+	text,
+	variant,
+	width,
+	height,
+	padding,
+	...props
+}: Props) => {
+	return (
+		<button
+			className={`${style.button} ${style[variant]}`}
+			style={{ width, height, padding }}
+			{...props}
+		>
+			{text}
+		</button>
+	);
 };
 
 export default Button;
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -3,8 +3,8 @@ import { Meta, Story } from "@storybook/react";
 import { Button, Props } from "../src/components/Button/Button";
 
 const meta: Meta = {
-  title: "Button",
-  component: Button,
+	title: "Button",
+	component: Button,
 };
 export default meta;
 
@@ -13,25 +13,15 @@ const Template: Story<Props> = (args) => <Button {...args} />;
 export const Normal = Template.bind({});
 
 Normal.args = {
-  variant: "Default",
-  buttonText: "Button Text",
-  disabled: false,
+	variant: "Default",
+	text: "Button Text",
+	disabled: false,
 };
 
-export const Primary = () => (
-  <Button buttonText="Item" variant="primary"></Button>
-);
+export const Primary = () => <Button text="Item" variant="primary"></Button>;
 
 export const Secondary = () => (
-  <Button buttonText="Item" variant="secondary"></Button>
+	<Button text="Item" variant="secondary"></Button>
 );
 
-export const Hover = () => <Button buttonText="Item" variant="hover"></Button>;
-
-export const Active = () => (
-  <Button buttonText="Item" variant="active"></Button>
-);
-
-export const Critical = () => (
-  <Button buttonText="Item" variant="critical"></Button>
-);
+export const Critical = () => <Button text="Item" variant="critical"></Button>;
