3702ace9a759789ca1f5a483de759de3e6d4cb1c
Dominik Kukovec <kukovecd@krtech.co>
2022-08-22 14:32:13 +0200

    Changed type of import for button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { Button } from "../src/components/Button/Button";
 
 const Index = () => {
-	return (
-		<div className="App">
-			<a href="/">Homepage</a>
-			<br />
-			<a href="/about">Components</a>
-			<br />
-			<br />
-		</div>
-	);
+  return (
+    <div className="App">
+      <a href="/">Homepage</a>
+      <br />
+      <a href="/about">Components</a>
+      <br />
+      <br />
+    </div>
+  );
 };
 
 export default Index;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,30 +2,28 @@ import React, { HTMLAttributes } from "react";
 import style from "./Button.module.css";
 
 export interface Props extends HTMLAttributes<HTMLButtonElement> {
-	text: string;
-	variant: string;
-	width?: string;
-	height?: string;
-	padding?: string;
+  text: string;
+  variant: string;
+  width?: string;
+  height?: string;
+  padding?: string;
 }
 
 export const Button = ({
-	text,
-	variant,
-	width,
-	height,
-	padding,
-	...props
+  text,
+  variant,
+  width,
+  height,
+  padding,
+  ...props
 }: Props) => {
-	return (
-		<button
-			className={`${style.button} ${style[variant]}`}
-			style={{ width, height, padding }}
-			{...props}
-		>
-			{text}
-		</button>
-	);
+  return (
+    <button
+      className={`${style.button} ${style[variant]}`}
+      style={{ width, height, padding }}
+      {...props}
+    >
+      {text}
+    </button>
+  );
 };
-
-export default Button;
