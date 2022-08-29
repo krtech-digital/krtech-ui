93781cfe328858130b6e9cb797d7b423767ca9b0
erik <erik.kranjec8@outlook.com>
2022-08-29 13:08:14 +0200

    added value mapping to, fixed styling and sizing, added ...props

--- stories/Stepper.stories.tsx
+++ stories/Stepper.stories.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { Meta, Story } from "@storybook/react";
-import { Stepper, Props} from "../src/components/Stepper/Stepper";
+import { Stepper, Props } from "../src/components/Stepper/Stepper";
 
 const meta: Meta = {
-    title: "Stepper",
-    component: Stepper
-}
+	title: "Stepper",
+	component: Stepper,
+};
 
 export default meta;
 
-export const Default = () =>  <Stepper minValue={0} maxValue={100}></Stepper>
+export const Default = () => (
+	<Stepper minValue={0} maxValue={100} initialValue={0}></Stepper>
+);
+
+export const WithMap = () => (
+	<Stepper
+		minValue={-1}
+		maxValue={3}
+		initialValue={0}
+		counterMap={{
+			[-1]: "minus one",
+			0: "zero",
+			1: "one",
+			2: "two",
+			3: "three",
+		}}
+		style={{ width: "10rem" }}
+	></Stepper>
+);
