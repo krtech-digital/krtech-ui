c6968f3ad836a786b70acd227f04a2b1f39425b5
Dominik Kukovec <kukovecd@krtech.co>
2022-08-29 07:41:07 +0200

    Small changes

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -29,24 +29,6 @@ export const Select = ({ placeholder, options }: Props) => {
         style={{ borderColor: border ? "#195273" : "#D7D7D7" }}
       >
         <div>{value}</div>
-        <svg
-          style={{
-            transform: showOptions
-              ? "rotate(180deg) scale(2)"
-              : "rotate(0deg) scale(2)",
-            transition: "transform 0.25s",
-          }}
-          width="6"
-          height="4"
-          viewBox="0 0 6 4"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M3 2.00035L4.99953 -4.3732e-08L6 0.999823L3 4L4.33134e-07 0.999823L1.00047 -2.18536e-07L3 2.00035Z"
-            fill="#195273"
-          />
-        </svg>
       </div>
       {showOptions ? (
         <div className={style.options}>
